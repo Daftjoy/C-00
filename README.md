49648890a28a05a47dc0dfddd6d0c860665a1bd0
mac <mac@MacBook-Pro-de-mac.local>
2021-06-17 13:22:26 +0200

    Let's beggin

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my  solution to project C00 from the 42 School C++ piscine. Use make inside each folder to compile and test it
---
+++ ex01/phonebook.cpp
@@ -0,0 +1,33 @@
+#include "phonebook.hpp"
+
+int main()
+{
+    std::string opt;
+    int exit;
+    
+    exit = 1;
+
+    while (exit)
+    {
+        std::cout << "Please choose to ADD a new contact, SEARCH in the phonebook or EXIT this setup: ";
+        std::getline(std::cin, opt);
+        
+        if (opt == "EXIT")
+        {
+            std::cout << "Exiting program, thank you for trusting us!!" << std::endl;
+            exit = 0;
+        }
+        else if (opt == "ADD")
+            std::cout << "Ahora te lo hago" << std::endl;
+        else if (opt == "SEARCH")
+            std::cout << "Te lo busco" << std::endl;
+        else
+        {
+            std::cout << opt;
+            std::cout << " is not a valid option." << std::endl;
+        }
+    }
+
+
+
+}
